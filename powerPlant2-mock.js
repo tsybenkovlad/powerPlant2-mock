36f2c120c42fc28789c262e1cbee25779ae7358e
Tsybenko Vlad <tsybenkovlad.29@gmail.com>
2024-12-25 18:53:34 +0200

    data for server

--- powerPlant2-mock.js
+++ powerPlant2-mock.js
@@ -2,47 +2,39 @@ import express from "express"
 import { format } from 'date-fns'
 const app = express();
 app.use(express.json());
-app.get("/powerPlant", (request, response) => {
-    function getRandomIntInclusive(min, max) {
-        const minCeiled = Math.ceil(min);
-        const maxFloored = Math.floor(max);
-        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+function fill(str, char, n) {
+    let m = n - str.length;
+    let f = "";
+    for (let i = 0; i < m; i++) {
+        f+=char
     }
+    return f + str
+}
+app.get("/info", (request, response) => {
     let first = getRandomIntInclusive(0, 356)
-    let formatFirst;
-    if (String(first).length === 1) {
-        formatFirst = "00" + first
-    }else if(String(first).length === 2) {
-        formatFirst = "0" + first
-    }else  {
-        formatFirst = first
-    }
+    let formatFirst = fill(String(first), "0", 3);
     let second = getRandomIntInclusive(0, 23)
-    let formatSecond;
-    if (String(second).length === 1) {
-        formatSecond = "0" + second
-    }else {
-        formatSecond = second
-    }
+    let formatSecond = fill(String(second), "0", 2);
     let third = getRandomIntInclusive(0, 59)
-    let formatThird;
-    if (String(third).length === 1) {
-        formatThird = "0" + third
-    }else {
-        formatThird = third
-    }
+    let formatThird = fill(String(third), "0", 2);
+    let date = new Date();
     response.send({"appVersion": getRandomIntInclusive(0, 500),
         "espVersion": "v5.2.2-dirty",
         "uptime": formatFirst + "." + formatSecond + ":" + formatThird,
         "heapPercent": getRandomIntInclusive(0, 100),
         "heapBytes": getRandomIntInclusive(0, 35000),
         "rtc": {
-            "year": Number(format(new Date(),"yyyy")),
-            "month": Number(format(new Date(),"MM")),
-            "day": Number(format(new Date(),"d")),
-            "hour": Number(format(new Date(),"HH")),
-            "minute": Number(format(new Date(),"mm")),
-            "second": Number(format(new Date(),"ss"))
+            "year": Number(format(date,"yyyy")),
+            "month": Number(format(date,"MM")),
+            "day": Number(format(date,"d")),
+            "hour": Number(format(date,"HH")),
+            "minute": Number(format(date,"mm")),
+            "second": Number(format(date,"ss"))
         }});
 });
 app.listen(8080, () => {
